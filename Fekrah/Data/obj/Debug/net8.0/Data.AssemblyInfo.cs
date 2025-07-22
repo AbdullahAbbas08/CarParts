66212564aed0c72a5c025fdbc772e48ce418e21f
Muhammad-Shaaban <mshaaban@tafeel.com>
2025-07-22 10:00:10 +0300

    .

--- Fekrah/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Fekrah/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b3310921bd9ee121444322b48e577c716341601")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4585839cd123513f70f5fb827d418a9205119a44")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
